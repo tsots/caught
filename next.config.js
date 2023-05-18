e09d2b3d41b453288f8caee9d2ca4e8dfde0bc7f
tsots <mosalatsotetsi@gmail.com>
2023-05-18 10:28:13 +0200

    trial hoting on github

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  //   basePath: "/caught",
+  experimental: {
+    mdxRs: true,
+  },
+};
 
-module.exports = nextConfig
+const withMDX = require("@next/mdx")();
+module.exports = withMDX(nextConfig);
